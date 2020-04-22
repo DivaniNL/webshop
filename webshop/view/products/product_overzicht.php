9a2fb84224f8ad3f173e31018c22c11789447cba
DivaniNL <60508913+DivaniNL@users.noreply.github.com>
2020-04-22 15:20:44 +0200

    Add files via upload

--- webshop/view/products/product_overzicht.php
+++ webshop/view/products/product_overzicht.php
@@ -9,8 +9,8 @@ $products="<div class='product-flex'>";
 $products3="<div class='product-flex'>";
 
 while ($row = $result->fetch_assoc()) {
-$products.= "<div class='product-box'><a class='detail_href' href='product_info.php?product_id=".$row['product_id']."'><div class='product_img_div'><img class='product_img' src='admin/view/products/upload/".$row['image']."'></div><p class='title'>".$row['name']."</p><br><p class='description'>".$row['description']."</p><br><p class='description'>Prijs: ".$row['price']."</p><br><p class='description'>Gewicht: ".$row['weight']." gram</p></a></div>";
-$products3.= "<div class='product-box'><div class='product_img_div'><img class='product_img' src='admin/view/products/upload/".$row['image']."'></div><p class='title'>".$row['name']."</p><br><p class='description'>".$row['description']."</p><br><p class='description'>Prijs: ".$row['price']."</p><br><p class='description'>Gewicht: ".$row['weight']." gram</p></div>";
+$products.= "<div class='product-box'><a class='detail_href' href='product_info.php?product_id=".$row['product_id']."'><div class='product_img_div'><img class='product_img' src='admin/view/products/upload/".$row['image']."'></div><p class='title'>".$row['name']."</p><p class='description'>".$row['description']."</p><p class='description'>Prijs: ".$row['price']."</p><p class='description'>Gewicht: ".$row['weight']." gram</p></a></div>";
+$products3.= "<div class='product-box'><div class='product_img_div'><img class='product_img' src='admin/view/products/upload/".$row['image']."'></div><p class='title'>".$row['name']."</p><p class='description'>".$row['description']."</p><p class='description'>Prijs: ".$row['price']."</p><p class='description'>Gewicht: ".$row['weight']." gram</p></div>";
 }
 $productsa.="</div>";
 $products.="</div>";
